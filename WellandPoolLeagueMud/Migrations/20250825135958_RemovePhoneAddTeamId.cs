a17d89bd2363ab7b1c24721478029b2816d1656f
Chris Wiley <sithlord2k@gmail.com>
2025-09-06 18:31:53 -0400

    Commit

---
+++ WellandPoolLeagueMud/Migrations/20250825135958_RemovePhoneAddTeamId.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WellandPoolLeagueMud.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovePhoneAddTeamId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "WPLMud_Players");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TeamId",
+                table: "WPLMud_Players",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WPLMud_Players_TeamId",
+                table: "WPLMud_Players",
+                column: "TeamId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WPLMud_Players_WPLMud_Teams_TeamId",
+                table: "WPLMud_Players",
+                column: "TeamId",
+                principalTable: "WPLMud_Teams",
+                principalColumn: "TeamId",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WPLMud_Players_WPLMud_Teams_TeamId",
+                table: "WPLMud_Players");
+
+            migrationBuilder.DropIndex(
+                name: "IX_WPLMud_Players_TeamId",
+                table: "WPLMud_Players");
+
+            migrationBuilder.DropColumn(
+                name: "TeamId",
+                table: "WPLMud_Players");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "WPLMud_Players",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: true);
+        }
+    }
+}
